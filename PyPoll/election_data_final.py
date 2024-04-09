50451bfe9f4d4cb31990bb226adb843fe8576b07
ChairLeg39 <ajlee39@gmail.com>
2024-04-08 22:35:51 -0500

    Updated both PyPoll and PyBank with test prints to terminal to meet the goals for the assignment. Created separate _notes.py and _final.py. Please reference the _final.py files for assessing the final grade.

--- PyPoll/election_data_final.py
+++ PyPoll/election_data_final.py
@@ -1,4 +1,4 @@
-#  import csv module
+# import csv module
 import csv
 
 # set path for file
@@ -8,7 +8,7 @@ election_file_path = "PyPoll/Resources/election_data.csv"
 total_votes = 0
 candidates = []
 header = []
-candidates_votes = {} # key is candidate name, value is vote count
+candidates_votes = {}
 votes = 0
 max_votes = 0
 percentage = 0
@@ -21,17 +21,15 @@ with open(election_file_path, 'r') as election_file:
     # store contents of the csv file into csv_file
     csv_file = csv.reader(election_file)
 
-    # skips the header row, stores column headers in header
+    # skips the header row
     header = next(csv_file)
 
-    # read a row in the file, loop through each row
+    # read a row in the file
     for row in csv_file:
         
-        # add to total votes
         # counts the number of rows to sum up total votes
         total_votes += 1
 
-        # "Ballot ID,County,Candidate"
         ballot_id = row[0]
         county = row[1]
         candidate = row[2]
@@ -41,35 +39,44 @@ with open(election_file_path, 'r') as election_file:
             # add to list if they haven't been added to the list
             candidates.append(candidate)
 
-        # check to see if the candidate exists in the dictionary (same as comic book)
+        # check to see if the candidate exists in the dictionary
         if candidate not in candidates_votes:
             candidates_votes[candidate] = 1
         else:
             # sum up votes for each candidate
             candidates_votes[candidate] += 1
 
-# search for candidate name in the candidate_votes dict (to calc % of votes and winner)
+# test print total_votes to terminal
+print(total_votes)
+
+# test print candidate names to terminal
+print(candidates)
+
+# test print dict of candidate name and votes
+print(candidates_votes)
+
+# search for candidate name in the candidate_votes dict
 for candidate in candidates_votes.keys():
 
     # store vote count for each candidate as int
     votes = candidates_votes[candidate]
-    # print(candidate) prints candidate names
-    # print(votes) prints the votes count as int
-    # print(candidates_votes) prints the whole dict as key value pairs
 
     # calc % of votes
     percentage = (votes / total_votes) * 100
 
-    # store percentage in a new dict, key is candidate name and value is percentage
+    # store percentage in a new dict
     candidates_percentage[candidate] = percentage
-   
+ 
+    # test print candidate percentage of votes to terminal
+    print(candidates_percentage)
+
     # calc winner based on popular vote
     if votes > max_votes:
         max_votes = votes
         winner = candidate
-
-# candidates_votes = {'Charles Casper Stockham': 85213, 'Diana DeGette': 272892, 'Raymon Anthony Doane': 11606}
-# candidates_percentages = {'Charles Casper Stockham': 23.049, 'Diana DeGette': 73.812, 'Raymon Anthony Doane': 3.139}
+    
+    # test print winner to terminal
+    print(winner)
 
 out_file_path = "PyPoll/Analysis/election_data_analysis.txt"
 
